c4e9934cb506c57ae0eb353d6d5fd2ad85d65b09
Michael Noukhovitch <mnoukhov@gmail.com>
2024-06-28 23:39:43 +0000

    train gen and eval script

---
+++ train_generate_and_eval.sh
@@ -0,0 +1,6 @@
+accelerate launch --config_file configs/deepspeed_zero2.yaml --mixed_precision=fp16 --num_processes $GPU $@
+MODEL_PATH=$(readlink -f output_dir)
+echo "Using output dir symlinked: $MODEL_PATH"
+OUTPUT_ARG="--model_name_or_path $MODEL_PATH"
+python generate_for_eval.py --config configs/generate_tldr.yml $OUTPUT_ARG && \
+accelerate launch --multi_gpu --mixed_precision=fp16 --num_processes=$GPU load_and_eval.py --config configs/evaluate_tldr.yml $OUTPUT_ARG

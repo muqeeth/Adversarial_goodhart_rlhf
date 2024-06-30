eae16ef664ceaa973577be66dd17a2a9f2a7419a
Michael Noukhovitch <mnoukhov@gmail.com>
2024-06-30 05:50:06 +0000

    correct saving steps

--- src/online_dpo_trainer.py
+++ src/online_dpo_trainer.py
@@ -287,7 +287,7 @@ class OnlineDPOTrainer(RLOOTrainer):
         entropy_stats = torch.zeros(stats_shape, device=device)
         ratio_stats = torch.zeros(stats_shape, device=device)
         model.train()
-        self.state.max_steps = args.num_updates
+        self.state.max_steps = args.num_updates * args.num_mini_batches
         self.state.num_train_epochs = args.total_episodes / self.train_dataset_len
         self.state.is_local_process_zero = self.is_local_process_zero()
         self.state.is_world_process_zero = self.is_world_process_zero()

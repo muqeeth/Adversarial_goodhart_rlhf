845d46e9c4a74bbfd93ffa2a54a6bdddb1a474a9
Michael Noukhovitch <mnoukhov@gmail.com>
2024-07-03 04:52:17 +0000

    train sft with fp32

--- train_generate_and_eval.sh
+++ train_generate_and_eval.sh
@@ -1,4 +1,4 @@
-accelerate launch --config_file configs/deepspeed_zero2.yaml --mixed_precision=fp16 --num_processes $GPU $@
+accelerate launch --config_file configs/deepspeed_zero2.yaml --mixed_precision=${FP:=fp16} --num_processes $GPU $@
 MODEL_PATH=$(readlink -f output_dir)
 echo "Using output dir symlinked: $MODEL_PATH"
 OUTPUT_ARG="--model_name_or_path $MODEL_PATH"

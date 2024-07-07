152b1926b08323fe7e7ab8b5964e1f0ed40779f3
Michael Noukhovitch <mnoukhov@gmail.com>
2024-07-07 21:51:28 +0000

    correct generation and eval for peft

--- train_generate_and_eval.sh
+++ train_generate_and_eval.sh
@@ -3,17 +3,22 @@ accelerate launch --config_file configs/deepspeed_zero2.yaml --mixed_precision=$
 MODEL_PATH=$(readlink -f output_dir)
 echo "Using output dir symlinked: $MODEL_PATH"
 OUTPUT_ARG="--model_name_or_path $MODEL_PATH"
-python generate_for_eval.py --config configs/generate_tldr.yml $OUTPUT_ARG && \
+
+if [[ "$MODEL_PATH" == *"pythia2.8b"* ]]; then
+    PEFT_ARG = " --base_model_name mnoukhov/pythia2.8b-sft-tldr"
+else
+    PEFT_ARG = ""
+fi
+python generate_for_eval.py --config configs/generate_tldr.yml $OUTPUT_ARG $PEFT_ARG
 
 if [[ "$MODEL_PATH" == *"pythia410m"* ]]; then
-    OUTPUT_ARG="$OUTPUT_ARG --ref_model_name mnoukhov/pythia410m-sft-tldr"
+    REF_ARG=" --ref_model_name mnoukhov/pythia410m-sft-tldr"
 elif [[ "$MODEL_PATH" == *"pythia1b"* ]]; then
-    OUTPUT_ARG="$OUTPUT_ARG --ref_model_name mnoukhov/pythia1b-sft-tldr"
+    REF_ARG=" --ref_model_name mnoukhov/pythia1b-sft-tldr"
 elif [[ "$MODEL_PATH" == *"pythia2.8b"* ]]; then
-    OUTPUT_ARG="$OUTPUT_ARG --ref_model_name mnoukhov/pythia2.8b-sft-tldr --base_model_name mnoukhov/pythia2.8b-sft-tldr"
+    REF_ARG=" --ref_model_name mnoukhov/pythia2.8b-sft-tldr"
 else
     echo "output path doesn't contain one of model names"
     exit 1
 fi
-echo "evaluating using base model: $MODEL"
-accelerate launch --multi_gpu --mixed_precision=fp16 --num_processes=$NPROC load_and_eval.py --config configs/evaluate_tldr.yml $OUTPUT_ARG
+accelerate launch --multi_gpu --mixed_precision=fp16 --num_processes=$NPROC load_and_eval.py --config configs/evaluate_tldr.yml $OUTPUT_ARG $REF_ARG

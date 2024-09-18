ac0dbdd101de57d47c208c972f46149011b57c6e
Michael Noukhovitch <noukhovm@mila.quebec>
2024-09-18 17:41:15 -0400

    get rtx8000 to use vllm

--- train_one_mila.sh
+++ train_one_mila.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=trl_summarize
-#SBATCH --output=job_output.txt
-#SBATCH --error=job_error.txt
+#SBATCH --output=results/%j/job_output.txt
+#SBATCH --error=results/%j/job_error.txt
 #SBATCH --time=10:00:00
 #SBATCH --mem=32Gb
-#SBATCH --gres=gpu:1
-#SBATCH --cpus 4
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-node=1
 
 source mila.sh
 $@

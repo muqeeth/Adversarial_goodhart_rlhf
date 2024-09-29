3362fb00c175308d97eaccf5d7f1494a13f2abcf
Michael Noukhovitch <noukhovm@mila.quebec>
2024-09-29 14:31:08 -0400

    just set torch dtype to float16 for load and eval

--- gen_eval_mila.sh
+++ gen_eval_mila.sh
@@ -11,7 +11,6 @@
 set -e
 source mila.sh
 MODEL_PATH_ARG=$@
-: ${DTYPE_ARG:=""}
 
 if [[ "$MODEL_PATH_ARG" == *"pythia2.8b"* ]] && [[ "$MODEL_PATH_ARG" == *"lora"* ]]; then
     PEFT_ARG=" --base_model_name mnoukhov/pythia2.8b-sft-tldr"
@@ -24,7 +23,7 @@ else
 fi
 echo $PEFT_ARG
 
-python generate_for_eval.py --config configs/generate_tldr.yml $MODEL_PATH_ARG $PEFT_ARG $DTYPE_ARG
+python generate_for_eval.py --config configs/generate_tldr.yml $MODEL_PATH_ARG $PEFT_ARG
 
 if [[ "$MODEL_PATH_ARG" == *"pythia410m"* ]]; then
     REF_ARG=" --ref_model_name mnoukhov/pythia410m-sft-tldr"
@@ -37,4 +36,4 @@ else
     exit 1
 fi
 
-python load_and_eval.py --config configs/evaluate_tldr.yml $MODEL_PATH_ARG $REF_ARG
+python load_and_eval.py --config configs/evaluate_tldr.yml $MODEL_PATH_ARG $REF_ARG --torch_dtype float16
